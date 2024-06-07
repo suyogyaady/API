df0661c20e587ef4c9f8c4d0e12ed75b6e5f4abd
suyogya dhoj <suyogyaadi@gmail.com>
2024-06-07 10:52:31 +0545

    update product done

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const productModel = require('../models/productModel');
 const Product = require('../models/productModel');
+const fs = require('fs')
 
 const createProduct = async (req, res) => {
 
@@ -91,9 +92,9 @@ const getAllProducts = async (req, res) => {
             "error": error
         })
     }
-    
 
-    
+
+
 }
 
 // fetch single products
@@ -105,18 +106,18 @@ const getSingleProduct = async (req, res) => {
     //find
     try {
         const product = await Product.findById(productId)
-        if(!product){
+        if (!product) {
             res.status(400).json({
-                "success" : false,
-                "message" : "No Product Found",
+                "success": false,
+                "message": "No Product Found",
             })
         }
         res.status(201).json({
-            "success" : true,
-            "message" : "Product fetched",
-            "product" : product
+            "success": true,
+            "message": "Product fetched",
+            "product": product
         })
-        
+
     } catch (error) {
         console.log(error)
         res.status(500).json({
@@ -124,7 +125,99 @@ const getSingleProduct = async (req, res) => {
             "message": "Interval server error",
             "error": error
         })
-        
+
+    }
+
+}
+
+//delete product
+const deleteProduct = async (req, res) => {
+
+    try {
+
+        await productModel.findByIdAndDelete(req.params.id)
+        res.status(201).json({
+            "success": true,
+            "message": "Product Deleted Successfully",
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            "success": false,
+            "message": "Internal Server Error",
+            "error": error
+        })
+
+    }
+
+}
+
+// <----update product---->
+// 1. get product id (URL)
+// 2. if image :
+// 3. New image should be uploadee
+// 4. old image should be deleted
+//  5. final product (database) product image
+//  6. find that image in directory
+//  7. Delete the image
+// 8. Update that product
+
+const updateProduct = async (req, res) => {
+
+    try {
+
+        //if there is image
+        if (req.files && req.files.productImage) {
+
+            //Destructuring
+            const { productImage } = req.files;
+
+            //upload image to /public/products folder
+            // 1. Generate new image name (abc.png) -> (21324-abc.png)
+            const imageName = `${Date.now()}-${productImage.name}`;
+
+            // 2. Make a upload path(/path/upload- directory)
+            const imageUploadPath = path.join(__dirname, `../public/products/${imageName}`)
+
+            //Move to folder
+            await productImage.mv(imageUploadPath)
+
+            //req.params (id), req.body(updated data : pm, pp, pc, pd), req.files(image)
+            //add new field to req.body (productImage -> name)
+            req.body.productImage = imageName; //image uploaded (generated name)
+
+            //if image is uploaded and req.body is assingned
+            if(req.body.productImage){
+
+                // finding existing product
+                const existingProduct = await productModel.findById(req.params.id)
+
+                //searching in the directory/folder
+                const oldImagePath =  path.join(__dirname, `../public/products/${existingProduct.productImage}`)
+
+                // delete from file system
+                fs.unlinkSync(oldImagePath)
+            }
+        }
+
+        // update the data
+        const updatedProduct = await productModel.findByIdAndUpdate(req.params.id, req.body)
+        res.status(201).json({
+            success : true,
+            message : "Product updated !",
+            product : updatedProduct
+        })
+
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            error: error
+        })
+
     }
 
 }
@@ -133,4 +226,6 @@ module.exports = {
     createProduct,
     getAllProducts,
     getSingleProduct,
+    deleteProduct,
+    updateProduct
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -12,5 +12,11 @@ router.get('/get_all_products', productController.getAllProducts)
 // fetch single products
 router.get('/get_single_products/:id', productController.getSingleProduct)
 
+//delete product
+router.delete('/delete_product/:id',productController.deleteProduct)
+
+//update product
+router.put('/update_product/:id',productController.updateProduct)
+
 module.exports = router
    
