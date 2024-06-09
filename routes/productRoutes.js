5f62c00b486ef76c949c93e240b53aca1c6ed499
suyogya dhoj <suyogyaadi@gmail.com>
2024-06-09 11:42:15 +0545

    authorization guard in dashboard and update

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,16 +1,17 @@
 
 
 const router = require('express').Router();
-const productController = require('../controllers/productControllers')
+const productController = require('../controllers/productControllers');
+const { authGuard } = require('../middleware/authGuard');
 
 
 router.post('/create', productController.createProduct)
 
 // fetch all products
-router.get('/get_all_products', productController.getAllProducts)
+router.get('/get_all_products', authGuard, productController.getAllProducts)
 
 // fetch single products
-router.get('/get_single_products/:id', productController.getSingleProduct)
+router.get('/get_single_products/:id',authGuard, productController.getSingleProduct)
 
 //delete product
 router.delete('/delete_product/:id',productController.deleteProduct)
